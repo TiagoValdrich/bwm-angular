c18454599d52c016f2020fe50193cf51d3395d71
Tiago Valdrich <tiago.valdrich@hotmail.com>
2018-11-16 19:12:08 -0200

    Organizing the project, separating the rentals in different components, creating the service for providing the data for the components and introducing Observables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bwm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bwm-angular';
 }
---
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../shared/rental.service';
+
+@Component({
+  selector: 'bwm-rental-list',
+  templateUrl: './rental-list.component.html',
+  styleUrls: ['./rental-list.component.scss']
+})
+export class RentalListComponent implements OnInit {
+
+  public rentals: any
+
+  constructor(private rentalService: RentalService) { }
+
+  ngOnInit() {
+    const rentalObservable = this.rentalService.getRentals();
+
+    rentalObservable.subscribe(
+      (rentals) => {
+        this.rentals = rentals
+      },
+      (err) => {
+        console.log("Error")
+      }, 
+      () => {
+        console.log('Complete')
+      }
+    );
+  }
+
+}
